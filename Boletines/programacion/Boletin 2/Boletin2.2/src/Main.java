62feaa7f16ebd481cb6db892c3b2c99cd7706605
Samuel Hermida <Shermidagregores@danielcastelao.org>
2024-12-10 11:09:54 +0100

    h

---
+++ Boletines/programacion/Boletin 2/Boletin2.2/src/Main.java
@@ -0,0 +1,9 @@
+class Boletin2_2{
+    public static void main(String[] args) {
+        double lado, area ;
+
+        lado = 3 ;
+        area = lado * lado ;
+        System.out.println(area);
+    }
+}
