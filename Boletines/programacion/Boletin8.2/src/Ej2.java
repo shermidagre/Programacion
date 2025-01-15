ea498ff8fd28fbede2a10ea592877efc1ed21085
Samuel Hermida <Shermidagregores@danielcastelao.org>
2025-01-15 09:44:05 +0100

    avanzar..

--- Boletines/programacion/Boletin8.2/src/Ej2.java
+++ Boletines/programacion/Boletin8.2/src/Ej2.java
@@ -81,4 +81,15 @@ public class Ej2 {
 
             sc.close();
     }
+    public static void imprimirporcolores(){
+        String str = "Hola, mundo!";char[] charArray = str.toCharArray();
+        // Códigos ANSI para colores
+        String[] colors = {"\u001B[31m", "\u001B[32m", "\u001B[33m", "\u001B[34m", "\u001B[35m", "\u001B[36m", "\u001B[0m"};
+
+        for (int i = 0; i < charArray.length; i++) {
+
+            System.out.print(colors[i % colors.length] + charArray[i] + "\u001B[0m"); // Cambiar de color
+
+        }
+    }
 }
