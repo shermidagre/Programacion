c00b5242bf7be619c2005a8e49379d6d82b690e4
Samuel Hermida <Shermidagregores@danielcastelao.org>
2025-01-16 13:37:05 +0100

    retoques

--- Programas random/Aforcado/src/Main.java
+++ Programas random/Aforcado/src/Main.java
@@ -1,6 +1,14 @@
 import java.util.Scanner;
  class Aforcado {
 
+     public static final String ANSI_GREEN = "\u001B[32m"; // Verde
+
+     public static final String ANSI_RED = "\u001B[31m";   // Rojo
+
+     public static final String ANSI_BLUE= "\u001B[34m"; // Azul
+
+     public static final String ANSI_RESET = "\u001B[0m";  // Restablecer color
+
      public static void main (String [] args) {
          String palabraAdivinhar = solicitaPalabra();
          String cadeaGuions = cadeaGuions(palabraAdivinhar);
@@ -22,11 +30,68 @@ import java.util.Scanner;
                  else System.out.println("A letra non e correcta");
                  contfa ++;
              }
-             if(contfa==1) System.out.println("Llevas un total de "+ contfa +"fallo");
-             if(contfa==2) System.out.println("LLevas un total de"+contfa+"fallos");
+             if(contfa==1) System.out.println("Llevas "+ contfa +"fallo");
+             System.out.println("Se ha quedado sin piernas");
+             System.out.println("__");
+             System.out.println(" | ");
+             System.out.println(" O ");
+             System.out.println("/|\\");
+             System.out.println(ANSI_RED+"/ \\"+ANSI_RESET);
+             System.out.println();
              ronda++;
+             if(contfa==2) System.out.println("LLevas un total de"+contfa+"fallos");
+             System.out.println("Se ha quedado sin los brazos");
+             System.out.println(         "__");
+             System.out.println(         " | ");
+             System.out.println(         " O ");
+             System.out.println(ANSI_RED+"/|\\"+ANSI_RESET);
+             System.out.println(ANSI_RED+"/ \\"+ANSI_RESET);
+             ronda++;
+             if(contfa==3) System.out.println("LLevas un total de"+contfa+"fallos");
+             System.out.println("Ya lo estas dejando tieso, se ha quedado sin pechito");
+             System.out.println("__");
+             System.out.println(" | ");
+             System.out.println(ANSI_RED+" O "+ANSI_RESET);
+             System.out.println(ANSI_RED+"/|\\"+ANSI_RESET);
+             System.out.println(ANSI_RED+"/ \\"+ANSI_RESET);
+             ronda++;
+             if(contfa==4) System.out.println("LLevas un total de"+contfa+"fallos");
+             System.out.println("Para un poco ya, esta te salvas");
+             System.out.println("__");
+             System.out.println(" | ");
+             System.out.println(" 0 ");
+             System.out.println(" | ");
+             System.out.println(ANSI_RED+" O "+ANSI_RESET);
+             System.out.println(ANSI_RED+"/|\\"+ANSI_RESET);
+             System.out.println(ANSI_RED+"/ \\"+ANSI_RESET);
+             ronda++;
+             if(contfa==5) System.out.println("LLevas un total de"+contfa+"fallos");
+             System.out.println("Pierde ya que el muñeco ya esta tieso");
+             System.out.println("Ya lo estas dejando tieso, un brazo menos");
+             System.out.println("__");
+             System.out.println(" | ");
+             System.out.println(" O ");
+             System.out.println("/|\\");
+             System.out.println("/ \\");
+             ronda++;
+             if(contfa==6) System.out.println("LLevas un total de"+contfa+"fallos");
+             System.out.println("Lo has dejado solo con la cabez y ya la tiene explotada de dar html");
+             System.out.println("Ya lo estas dejando tieso, un brazo menos");
+             System.out.println("__");
+             System.out.println(" | ");
+             System.out.println(" O ");
+             System.out.println("/|\\");
+             System.out.println("/ \\");
+             ronda++;
+             if(contfa==7) System.out.println("Has perdido, tienes un total de"+contfa+"fallos, dejaste al muñeco tieso");
+             System.out.println("Ha quedado tieso total parece la nunu de sandark");
+             System.out.println("__");
+             System.out.println(" | ");
+             System.out.println(" O ");
+             System.out.println("/|\\");
+             System.out.println("/ \\");
 
-         } while (ronda < 7 && acerto == false);
+         } while (ronda <= 6 && acerto == false);
          if (acerto) System.out.println("Felicidades acertaches a palabra: " + palabraAdivinhar);
          else System.out.println("Mágoa, non acertaches. A palabra era " + palabraAdivinhar);
          System.out.println("__");
