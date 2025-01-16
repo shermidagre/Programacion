e28fcff873626e6bfd82c531860f5096ba4d4855
Samuel Hermida <Shermidagregores@danielcastelao.org>
2025-01-16 14:53:35 +0100

    .

--- Boletines/Boletin8.2/src/ej10.java
+++ Boletines/Boletin8.2/src/ej10.java
@@ -1,16 +1,48 @@
 import java.util.Scanner;
 
 public class ej10 {
-    static void main(String[] args){
+    static void main(String[] args) {
 
-      Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-      System.out.println("Introduce una contraseña");
+        System.out.println("Introduce una contraseña");
+        String contraseña = sc.nextLine();
+        comprobarcontraseña(contraseña);
 
-      String contraseña= sc.next();
-      comprobarContraseña(contraseña);
     }
-    static void comprobarContraseña(String contraseña) {
+    static void comprobarcontraseña(String contraseña){
+        boolean correcto = false, maius = false, minus= false, numero=false;
+        char caracter;
+        if(contraseña.length()>7){
+
+            for(int i=0; i<contraseña.length(); i++){
+
+                caracter = contraseña.charAt(i); //Obtenemos caracter
+
+                if(Character.isUpperCase(caracter)){
+                    maius = true;
+                }
+                if(Character.isLowerCase(caracter)){
+                    minus = true;
+                }
+                if(Character.isDigit(caracter)){
+                    numero = true;
+                }
+
+            }
+            if(maius && minus && numero){
+                correcto = true;
+            }
+            if(correcto){
+                System.out.println("Contraseña válida");
+            }else{
+                System.out.println("Contraseña no válida");
+            }
+
+        }
+    }
+}
+   /* static void comprobarContraseña(String contraseña) {
         int longitud = contraseña.length();
         boolean mayusculas = false;
         boolean minusculas = false;
@@ -44,3 +76,4 @@ public class ej10 {
     }
 
 }
+*/
--- Boletines/Boletin8.2/src/ej11.java
+++ Boletines/Boletin8.2/src/ej11.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class ej11 {
+    static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+
+
+    }
+    public static String formatearnomes(String nome, String apelido){
+        String nomeApelido;
+
+        nomeApelido=nome.toLowerCase().trim();
+
+        return nomeApelido;
+
+    }
 }
