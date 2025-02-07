9defc1b5dc9f7c0f9cfc6c59ee35913938fd9f70
Samuel Hermida <Shermidagregores@danielcastelao.org>
2025-02-07 09:08:26 +0100

    .

---
+++ COD/Ideasobjetos/src/Main.java
@@ -0,0 +1,75 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        PersonajeDragonBall goku = new PersonajeDragonBall("Goku", 100, 500);
+
+        goku.realizarAtaqueEspecial();
+
+        goku.aumentarEnergia(50);
+
+        goku.cambiarTransformacion("Super Saiyan");
+
+        System.out.println(goku);
+
+    }
+
+}
+class PersonajeDragonBall {
+
+    private String nombre;
+
+    private int poderAtaque;
+
+    private int nivelEnergia;
+
+    private String transformacion;
+
+
+    public PersonajeDragonBall(String nombre, int poderAtaque, int nivelEnergia) {
+
+        this.nombre = nombre;
+
+        this.poderAtaque = poderAtaque;
+
+        this.nivelEnergia = nivelEnergia;
+
+        this.transformacion = "Normal";
+
+    }
+
+
+    public void realizarAtaqueEspecial() {
+
+        System.out.println(nombre + " realiza un ataque especial con poder de ataque: " + poderAtaque * 2);
+
+    }
+
+
+    public void aumentarEnergia(int cantidad) {
+
+        nivelEnergia += cantidad;
+
+        System.out.println(nombre + " ha aumentado su nivel de energía a: " + nivelEnergia);
+
+    }
+
+
+    public void cambiarTransformacion(String nuevaTransformacion) {
+
+        transformacion = nuevaTransformacion;
+
+        System.out.println(nombre + " se ha transformado en: " + transformacion);
+
+    }
+
+
+    @Override
+
+    public String toString() {
+
+        return "Personaje: " + nombre + ", Poder de Ataque: " + poderAtaque + ", Nivel de Energía: " + nivelEnergia + ", Transformación: " + transformacion;
+
+    }
+
+}
