6b441e33aa4767fece992ad5ace867f51e289a3a
Samuel Hermida <Shermidagregores@danielcastelao.org>
2025-02-19 09:51:24 +0100

    .

--- Poo_juegoguerra/src/Contacorrente.java
+++ Poo_juegoguerra/src/Contacorrente.java
@@ -1,8 +1,13 @@
+// Juego con las clases , creacion de una cuenta corrente basica
+
+
 public class Contacorrente {
-        int saldo;
+
+        double saldo;
         String Titular;
-        int nConta;
+        String nConta;
         String nif;
+        static int contadorContas;
 
     public Contacorrente() {
 
@@ -10,31 +15,80 @@ public class Contacorrente {
 
         Titular="Juan";
 
-        nConta=19210783;
+        nConta="19210783";
 
-        nif="12893Q8N";
+        nif="12893KOAOIPDJA8N";
 
+        nif.toUpperCase();
 
-    }
-    public static void Sacardinero(String nif, int nConta, int saldo){
+        contadorContas =0;
 
-
-    }
-    public static void Ingresardinero(String nif, int Nconta){
 
     }
-    public  static  void Mostrarinformacion(String nif, int nConta, int saldo, String Titular){
+    public Contacorrente(String Titular, String nConta, String nif, double saldo, int contadorContas){
+
+        this.Titular=Titular;
+        this.nConta=nConta;
+        setNif(nif);
+        setSaldo(saldo);
+        contadorContas++;
+
+    }
+
+    public void sacarDinero(double valor) {
+
+        if (valor > saldo) {
+
+            System.out.println("Saldo insuficiente.");
+
+        } else {
+
+            saldo -= valor;
+
+            System.out.println("Retiro de " + valor + " realizado. Saldo atual: " + saldo);
+
+        }
+
+    }
+
+
+    public void ingresarDinero(double valor) {
+
+        saldo += valor;
+
+        System.out.println("Depósito de " + valor + " realizado. Saldo atual: " + saldo);
+
+    }
+
+
+    public void mostrarInformacao() {
+
+        System.out.println("Titular: " + Titular);
+
+        System.out.println("Número da Conta: " + nConta);
+
+        System.out.println("NIF: " + nif);
+
+        System.out.println("Saldo: " + saldo);
 
     }
 
     // gett and sett
 
-    public int getSaldo() {
+    public double getSaldo() {
         return saldo;
     }
 
-    public void setSaldo(int saldo) {
+    public void setSaldo(double saldo) {
+
+        if (saldo < 0) {
+
+            throw new IllegalArgumentException("El saldo no debe ser negativo");
+
+        }
+
         this.saldo = saldo;
+
     }
 
     public String getTitular() {
@@ -45,11 +99,11 @@ public class Contacorrente {
         Titular = titular;
     }
 
-    public int getnConta() {
+    public String getnConta() {
         return nConta;
     }
 
-    public void setnConta(int nConta) {
+    public void setnConta(String nConta) {
         this.nConta = nConta;
     }
 
@@ -58,7 +112,70 @@ public class Contacorrente {
     }
 
     public void setNif(String nif) {
-        this.nif = nif;
+
+        if (esNifValido(nif)) {
+
+            this.nif = nif;
+
+        } else {
+
+            throw new IllegalArgumentException("NIF no válido");
+
+        }
+
     }
 
+    private boolean esNifValido(String nif) {
+
+        // Comprobar que el NIF no sea nulo y tenga la longitud correcta
+
+        if (nif == null || nif.length() != 9) {
+
+            return false;
+
+        }
+
+        // Comprobar que los primeros 8 caracteres sean dígitos
+
+        String numeros = nif.substring(0, 8);
+
+        String letra = nif.substring(8, 9).toUpperCase();
+
+
+        if (!numeros.matches("\\d{8}") || !letra.matches("[A-Z]")) {
+
+            return false;
+
+        }
+
+        // Calcular la letra del NIF
+
+        int numero = Integer.parseInt(numeros);
+
+        char letraCalculada = calcularLetraNif(numero);
+
+        // Comparar la letra calculada con la letra proporcionada
+
+        return letra.equals(String.valueOf(letraCalculada));
+
+    }
+
+
+    private char calcularLetraNif(int numero) {
+
+        // Array de letras correspondientes a los números
+
+        char[] letras = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
+
+        return letras[numero % 23];
+
+    }
+
+    public static int getcontadorContas() {
+        return contadorContas;
+    }
+
+    public static void setContadorContas(int contadorContas) {
+        contadorContas = contadorContas;
+    }
 }
