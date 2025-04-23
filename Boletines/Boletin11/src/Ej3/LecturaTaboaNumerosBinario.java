347a2d412f812ccc46cc8c03f1f87262ae9c67d2
Samuel Hermida <Shermidagregores@danielcastelao.org>
2025-04-23 09:04:47 +0200

    .

--- Boletines/Boletin11/src/Ej3/LecturaTaboaNumerosBinario.java
+++ Boletines/Boletin11/src/Ej3/LecturaTaboaNumerosBinario.java
@@ -3,31 +3,64 @@ package Ej3;
 import java.io.*;
 
 public class LecturaTaboaNumerosBinario {
-    public LecturaTaboaNumerosBinario(String rutaFicheiro) {
-        ObjectOutputStream fluxoSaidaImpares = null;
-        ObjectOutputStream fluxoSaidaPares = null;
+    public LecturaTaboaNumerosBinario(String rutaFicheiroImpares, String rutaFicheiroPares) {
+        ObjectInputStream fluxoSaidaImpares = null;
+        ObjectInputStream fluxoSaidaPares = null;
         try {
-            fluxoSaidaImpares = new ObjectInputStream(new FileOutputStream("numerosimpares.dat");
-            int numero;
+            fluxoSaidaImpares = new ObjectInputStream(new FileInputStream(rutaFicheiroImpares));
+            fluxoSaidaPares = new ObjectInputStream(new FileInputStream(rutaFicheiroPares));
+
+            System.out.println(" Números Impares ");
+
             while (true) {
-                numero = fluxoSaidaImpares.();
+                int numero = fluxoSaidaImpares.readInt();
                 System.out.println(numero);
             }
         } catch (FileNotFoundException e) {
-            System.out.println("Ficheiro non encontrado: " + e.getMessage());
+            System.out.println("Ficheiro de números impares non encontrado " + e.getMessage());
+
         } catch (EOFException e) {
-            System.out.println("No hay nada mas que leer " + e.getMessage());
+            System.out.println("Fin del archivo de números impares ");
+
         } catch (IOException e) {
-            System.out.println("Error de entrada/saída: " + e.getMessage());
+            System.out.println("Error de entrada/saída al leer números impares " + e.getMessage());
+
         } finally {
+
             if (fluxoSaidaImpares != null) {
                 try {
-                    fluxoSaidaImpares.close();
+                    fluxoSaidaImpares.close(); // cuando termine cerramos
                 } catch (IOException e) {
-                    System.out.println("Error: Problema ao fechar o fluxo de entrada" + e.getMessage());
+                    System.out.println("Error: Problema ao fechar o fluxo de entrada de números impares " + e.getMessage());
                 }
-            }  // end if
+            }
+        }
+        try {
+            System.out.println(" Números Pares ");
+            while (true) {
+                int numero = fluxoSaidaPares.readInt();
+                System.out.println(numero);
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Ficheiro de números pares non encontrado " + e.getMessage());
+
+        } catch (EOFException e) {
+            System.out.println("Fin del archivo de números pares ");
+
+        } catch (IOException e) {
+            System.out.println("Error de entrada/saída al leer números pares " + e.getMessage());
+
+        } finally {
+
+            if (fluxoSaidaPares != null) {
+                try {
+                    fluxoSaidaPares.close(); // cuando termine cerramos
+                } catch (IOException e) {
+                    System.out.println("Error: Problema ao fechar o fluxo de entrada de números pares " + e.getMessage());
+                }
+            }
         }
     }
-    }
+}
 
